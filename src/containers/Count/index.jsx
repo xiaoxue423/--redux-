8d714f7d97b5ec8c1789ffef9ae993014a6b3c83
Nora.Hu <nora.h@thinkwithu.com>
2023-01-04 14:22:05 +0800

    react-redux基本使用

--- src/containers/Count/index.jsx
+++ src/containers/Count/index.jsx
@@ -1,7 +1,28 @@
-import CountUI from '../../components/Count/index'
+import CountUI from "../../components/Count/index";
+import { connect } from "react-redux";
+import {createIncrementAction,createDecrementAction,createIncrementAsyncAction} from '../../redux/count_action'
 
-import {connect} from 'react-redux'
+function a(state) {
+  return { count: state };
+}
 
-const CountContainer = connect()(CountUI)
+function b(dispatch) {
+  return {
+    jia: (data) => {
+      // 通知redux执行加法
+      dispatch(createIncrementAction(data))
+    },
+    jian:(data) => {
+      // 通知redux执行减法
+      dispatch(createDecrementAction(data))
+    },
+    jiaAsync:(data,time) => {
+      // 通知redux执行异步加法
+      dispatch(createIncrementAsyncAction(data,time))
+    },
+  };
+}
 
-export default CountContainer
+const CountContainer = connect(a, b)(CountUI);
+
+export default CountContainer;
